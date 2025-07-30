e94b8dca52912489f3c13948bc6a1ce3d69e174e
Tectors <73559984+Tectors@users.noreply.github.com>
2025-07-29 22:07:12 -0700

    UpdateService Dialog

--- Source/vj0/Models/API/Responses/GitHubReleaseResponse.cs
+++ Source/vj0/Models/API/Responses/GitHubReleaseResponse.cs
@@ -5,5 +5,5 @@ namespace vj0.Models.API.Responses;
 public class GitHubReleaseResponse
 {
     [JsonProperty("name")] public string Name = null!;
-    [JsonProperty("html_url")] public string TagURL = null!;
+    [JsonProperty("html_url")] public string TagUrl = null!;
 }
--- Source/vj0/Services/UpdateService.cs
+++ Source/vj0/Services/UpdateService.cs
@@ -1,10 +1,12 @@
 using System;
+using CommunityToolkit.Mvvm.Input;
+using FluentAvalonia.UI.Controls;
 using vj0.Application;
 using vj0.Framework;
 
 namespace vj0.Services;
 
-public partial class UpdateService : IService
+public class UpdateService : IService
 {
     public async void Initialize()
     {
@@ -14,9 +16,35 @@ public partial class UpdateService : IService
         var latestVersion = new Version(latestRelease.Name);
         var currentVersion = new Version(Globals.VERSION);
         
-        if (true) // currentVersion < latestVersion)
+        if (true)
         {
+            var dialog = new ContentDialog
+            {
+                Title = "New Version Available!",
+                Content = $"Get the latest features and improvements for {Globals.APP_NAME}.",
+                CloseButtonText = "Dismiss",
+                PrimaryButtonText = "View Release Page",
+                PrimaryButtonCommand = new RelayCommand(() =>
+                {
+                    AppService.OpenLink(latestRelease.TagUrl);
+                })
+            };
             
+            _ = dialog.ShowAsync();
+        }
+
+        if (currentVersion > latestVersion)
+        {
+#if !DEBUG
+            var dialog = new ContentDialog
+            {
+                Title = "Development Build",
+                Content = $"You are currently running a developmental build of {Globals.APP_NAME}.\n\nThis issued version may be unstable.",
+                CloseButtonText = "Dismiss"
+            };
+            
+            _ = dialog.ShowAsync();
+#endif
         }
     }
 }
