f8b7b810f0a97e8a330c99ee712e9fe3b4431cb5
Tectors <73559984+Tectors@users.noreply.github.com>
2025-11-04 02:08:53 -0800

    Update Controller.cs

--- Source/vj0.Cloud/Controllers/Controller.cs
+++ Source/vj0.Cloud/Controllers/Controller.cs
@@ -30,7 +30,7 @@ public class CloudApiController : ControllerBase
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
     private static IEnumerable<BaseProfile> SecondaryBaseProfiles = [];
     private static BaseProfile? MainProfile;
-    private static bool IsBaseProfileReady => MainProfile!.Provider.Files.Count > 0;
+    private static bool IsBaseProfileReady => MainProfile != null && MainProfile!.Provider.Files.Count > 0;
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
     public static void SetProfile(BaseProfile profile)
